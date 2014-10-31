a4793f068bbebf301fd1de3508098b84460b7d82
Lautaro Orazi <orazile@gmail.com>
2014-10-31 14:31:37 -0300

    add rake file to run all tests

--- test/cassandro_test.rb
+++ test/cassandro_test.rb
@@ -5,12 +5,12 @@ Protest.describe "Cassandro Module" do
     SESSION.execute("DROP KEYSPACE IF EXISTS test_keyspace")
   end
 
-  test "must connect to database" do
+  test "connects to database" do
     client = Cassandro.connect(hosts: ["127.0.0.1"])
     assert_equal Cassandra::Session, client.class
   end
 
-  test "must create new keyspace" do
+  test "creates new keyspace" do
     Cassandro.connect(hosts: ["127.0.0.1"])
     assert_equal Cassandra::Results::Void, Cassandro.create_keyspace("test_keyspace").class
     assert_equal NilClass, Cassandro.use("test_keyspace").class
